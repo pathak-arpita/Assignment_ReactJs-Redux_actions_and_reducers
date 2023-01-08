6a1662366611e1d428999a4bbe579b75d763845c
Arpita <pathak.arpita6497@gmail.com>
2023-01-08 20:54:33 +0530

    latest push

--- src/reducers.js
+++ src/reducers.js
@@ -16,8 +16,27 @@ const reducer = (state = [],action = {}) => {
 export default reducer;
 */
 import { combineReducers } from "redux";
+import { LOGIN, LOGOUT } from "./actions";
 
 const initalState = {
     loggedIn : false,
     username : ''
 };
+
+const authReducer = (state=initalState,action) => {
+  switch(action.type){
+    case LOGIN : return {
+      loggedIn : true,
+      username : action.payload,
+    }
+    case LOGOUT : return {
+      loggedIn : false,
+      username : '',
+    }
+    default: return state
+  }
+}
+const reducers = combineReducers({
+  auth : authReducer
+})
+export default reducers;
